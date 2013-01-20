0c1fd445362cfb3b5bd79903ea1803f0b6d71b97
karlbennett <where_1ts@yahoo.com>
2013-01-20 18:32:36 +0000

    Created a package-info.java for the collections.builders package.

---
+++ src/main/java/collections/builders/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package contains all the builder collections, these are collections that can be given logic that will be
+ * evaluated within the collections constructor to build the elements that will be contained within the collection. The
+ * supplied logic is evaluated up front so must be completable to allow the collection to finish construction.
+ */
+package collections.builders;
